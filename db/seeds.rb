eec740596906e81804e61b3e1aba22722a8bf774
Destyka <102505452+Destyka@users.noreply.github.com>
2022-05-18 17:04:53 +0200

    rails

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+restaurant = Restaurant.new({name: 'MadonnaMe', address: 'Cours Balguerie', phone_number: '003367832876', category: 'italian'})
+  restaurant.save
+  restaurant = Restaurant.new({name: 'Alinea', address: 'Chicago', phone_number: '00167832876', category: 'french'})
+  restaurant.save
+  restaurant = Restaurant.new({name: 'French Fries Paradise', address: 'Brussels', phone_number: '0016876865876', category: 'belgian'})
+  restaurant.save
+  restaurant = Restaurant.new({name: 'Qanjude', address: 'Bordeaux', phone_number: '00336876865876', category: 'chinese'})
+  restaurant.save
+  restaurant = Restaurant.new({name: 'Sushi Pas Light', address: 'Tokyo', phone_number: '0046876865876', category: 'japanese'})
+  restaurant.save
